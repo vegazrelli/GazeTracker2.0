1dc1fcb0aaefd79f46e752b8ec2fdc72d8f836b9
Matteo Ciman <wizard88_mc@yahoo.it>
2013-04-04 15:20:21 +0200

    Updating project

---
+++ mscorlib.pdb/Alchemy/src/Alchemy/WebSocketClient.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using Alchemy.Classes;
+using Alchemy.Handlers.WebSocket.rfc6455;
+
+namespace Alchemy
+{
+    public class WebSocketClient
+    {
+        public TimeSpan ConnectTimeout = new TimeSpan(0, 0, 0, 10);
+        public bool IsAuthenticated;
+        public ReadyStates ReadyState = ReadyStates.CLOSED;
+        public string Origin;
+        public string[] SubProtocols;
+        public string CurrentProtocol { get; private set; }
+
+        public OnEventDelegate OnConnect = x => { };
+        public OnEventDelegate OnConnected = x => { };
+        public OnEventDelegate OnDisconnect = x => { };
+        public OnEventDelegate OnReceive = x => { };
+        public OnEventDelegate OnSend = x => { };
+        public OnEventDelegate OnFailedConnection = x => { };
+
+        private TcpClient _client;
+        private bool _connecting;
+        private Context _context;
+        private ClientHandshake _handshake;
+
+        private readonly string _path;
+        private readonly int _port;
+        private readonly string _host;
+
+        public enum ReadyStates
+        {
+            CONNECTING,
+            OPEN,
+            CLOSING,
+            CLOSED
+        }
+
+        public Boolean Connected
+        {
+            get
+            {
+                return _client != null && _client.Connected;
+            }
+        }
+
+        public WebSocketClient(string path)
+        {
+            var r = new Regex("^(wss?)://(.*)\\:([0-9]*)/(.*)$");
+            var matches = r.Match(path);
+
+            _host = matches.Groups[2].Value;
+            _port = Int32.Parse(matches.Groups[3].Value);
+            _path = matches.Groups[4].Value;
+        }
+
+        public void Connect()
+        {
+            if (_client != null) return;
+            
+            try
+            {
+                ReadyState = ReadyStates.CONNECTING;
+
+                _client = new TcpClient();
+                _connecting = true;
+                _client.BeginConnect(_host, _port, OnRunClient, null);
+
+                var waiting = new TimeSpan();
+                while (_connecting && waiting < ConnectTimeout)
+                {
+                    var timeSpan = new TimeSpan(0, 0, 0, 0, 100);
+                    waiting = waiting.Add(timeSpan);
+                    Thread.Sleep(timeSpan.Milliseconds);
+                }
+                if (_connecting) throw new Exception("Timeout");
+            }
+            catch (Exception)
+            {
+                Disconnect();
+                OnFailedConnection(null);
+            }
+        }
+
+        /// <summary>
+        /// Fires when a client connects.
+        /// </summary>
+        /// <param name="result">null</param>
+        protected void OnRunClient(IAsyncResult result)
+        {
+            try
+            {
+                _client.EndConnect(result);
+            }
+            catch (Exception)
+            {
+                Disconnect();
+                OnFailedConnection(null);
+                return;
+            }
+
+            using (_context = new Context(null, _client))
+            {
+                _context.BufferSize = 512;
+                _context.UserContext.DataFrame = new DataFrame();
+                _context.UserContext.SetOnConnect(OnConnect);
+                _context.UserContext.SetOnConnected(OnConnected);
+                _context.UserContext.SetOnDisconnect(OnDisconnect);
+                _context.UserContext.SetOnSend(OnSend);
+                _context.UserContext.SetOnReceive(OnReceive);
+                _context.UserContext.OnConnect();
+
+
+                while (_context.Connection.Connected)
+                {
+                    _context.ReceiveReady.Wait();
+
+                    try
+                    {
+                        _context.Connection.Client.BeginReceive(_context.Buffer, 0, _context.Buffer.Length, SocketFlags.None, DoReceive, _context);
+                    }
+                    catch (Exception)
+                    {
+                        break;
+                    }
+
+                    if (!IsAuthenticated)
+                    {
+                        Authenticate();
+                    }
+                }
+            }
+
+            Disconnect();
+        }
+
+        private void Authenticate()
+        {
+            _handshake = new ClientHandshake { Version = "8", Origin = Origin, Host = _host, Key = GenerateKey(), ResourcePath = _path, SubProtocols = SubProtocols};
+
+            _client.Client.Send(Encoding.UTF8.GetBytes(_handshake.ToString()));
+        }
+
+        private bool CheckAuthenticationResponse(Context context)
+        {
+            var receivedData = context.UserContext.DataFrame.ToString();
+            var header = new Header(receivedData);
+            var handshake = new ServerHandshake(header);
+
+            if (Authentication.GenerateAccept(_handshake.Key) != handshake.Accept) return false;
+
+            if (SubProtocols != null)
+            {
+                if (header.SubProtocols == null)
+                {
+                    return false;
+                }
+
+                foreach (var s in SubProtocols)
+                {
+                    if (header.SubProtocols.Contains(s) && String.IsNullOrEmpty(CurrentProtocol))
+                    {
+                        CurrentProtocol = s;
+                    }
+
+                }
+                if(String.IsNullOrEmpty(CurrentProtocol))
+                {
+                    return false;
+                }
+            }
+
+            ReadyState = ReadyStates.OPEN;
+            IsAuthenticated = true;
+            _connecting = false;
+            context.UserContext.OnConnected();
+            return true;
+        }
+
+        private void ReceiveData(Context context)
+        {
+            if (!IsAuthenticated)
+            {
+                var someBytes = new byte[context.ReceivedByteCount];
+                Array.Copy(context.Buffer, 0, someBytes, 0, context.ReceivedByteCount);
+                context.UserContext.DataFrame.Append(someBytes);
+                var authenticated = CheckAuthenticationResponse(context);
+                context.UserContext.DataFrame.Reset();
+
+                if (!authenticated)
+                {
+                    Disconnect();
+                }
+            }
+            else
+            {
+                context.UserContext.DataFrame.Append(context.Buffer, true);
+                if (context.UserContext.DataFrame.State == Handlers.WebSocket.DataFrame.DataState.Complete)
+                {
+                    context.UserContext.OnReceive();
+                    context.UserContext.DataFrame.Reset();
+                }
+            }
+        }
+
+        private void DoReceive(IAsyncResult result)
+        {
+            var context = (Context) result.AsyncState;
+            context.Reset();
+
+            try
+            {
+                context.ReceivedByteCount = context.Connection.Client.EndReceive(result);
+            }
+            catch (Exception)
+            {
+                context.ReceivedByteCount = 0;
+            }
+
+            if (context.ReceivedByteCount > 0)
+            {
+                ReceiveData(context);
+                context.ReceiveReady.Release();
+            }
+            else
+            {
+                context.Disconnect();
+            }
+        }
+
+        private static String GenerateKey()
+        {
+            var bytes = new byte[16];
+            var random = new Random();
+
+            for (var index = 0; index < bytes.Length; index++)
+            {
+                bytes[index] = (byte) random.Next(0, 255);
+            }
+
+            return Convert.ToBase64String(bytes);
+        }
+
+        public void Disconnect()
+        {
+            _connecting = false;
+
+            if (_client == null) return;
+            var dataFrame = new DataFrame();
+            dataFrame.Append(new byte[0]);
+
+            var bytes = dataFrame.AsFrame()[0].Array;
+
+            ReadyState = ReadyStates.CLOSING;
+
+            bytes[0] = 0x88;
+            if (_context != null && _context.UserContext != null)
+                _context.UserContext.Send(bytes);
+
+            _client.Close();
+            _client = null;
+            ReadyState = ReadyStates.CLOSED;
+            
+        }
+
+        public void Send(String data)
+        {
+            _context.UserContext.Send(data);
+        }
+
+        public void Send(byte[] data)
+        {
+            _context.UserContext.Send(data);
+        }
+    }
+}
---
+++ mscorlib.pdb/WSCom/WSClient.cs
@@ -0,0 +1,455 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using Alchemy;
+using Alchemy.Classes;
+using GazeTrackerClient;
+using GTCommons.Events;
+using Newtonsoft.Json;
+using System.Threading;
+
+namespace WSCom
+{
+    public class WSClient
+    {
+        #region VARIABLES
+
+        WebSocketClient wsClient;
+        static GazeTrackerClient.Client gtClient;
+
+        private static int IDSession = 0;
+        private bool connected = false;
+        private string logInfo;
+        private static UserContext cont;
+        private int screenWidth = 0;
+        private int screenHeight = 0;
+        private static int receivedWidth = 0;
+        private static int receivedHeight = 0;
+        private static bool onStream = false;
+
+        #endregion
+
+        #region EVENTS
+
+        public ErrorEventArgs e = null;
+        public delegate void ErrorEventHandler(object sender, ErrorEventArgs e);
+        public event ErrorEventHandler Error;
+
+        public delegate void ConnectionChangedHandler(object sender, StringEventArgs e);
+        public event ConnectionChangedHandler ConnectionChange;
+        
+        #endregion
+
+        #region CONSTRUCTOR
+
+        ///<summary>
+        /// Class Constructor that handles the Gazetracker client and the WebSocket connections.
+        /// </summary>
+        public WSClient()
+        {
+            createXml();
+            wsClient = new WebSocketClient("ws://ciman.math.unipd.it:8000/")
+            {
+                OnConnected = OnConnected,
+                OnDisconnect = OnDisconnect,
+                OnFailedConnection = OnFailedConnection,
+                OnReceive = OnReceive
+            };
+            gtClient = new GazeTrackerClient.Client();
+
+            gtClient.ErrorOccured += OnErrorOccured;
+
+            gtClient.Calibration.OnPointChange += new GazeTrackerClient.Calibration.PointChangeHandler(OnCalibrationPointChange);
+            gtClient.Calibration.OnEnd += new GazeTrackerClient.Calibration.EndHandler(OnCalibrationEnd);
+            gtClient.GazeData.OnGazeData += new GazeTrackerClient.GazeData.GazeDataHandler(OnGazeData);
+        }
+
+        ///<summary>
+        /// Class Constructor that handles the Gazetracker client and the WebSocket connections.
+        /// </summary>
+        /// <param name="address">The server address: ws://address:port/</param>
+        public WSClient(string address)
+        {
+            createXml();
+            wsClient = new WebSocketClient(address)
+            {
+                OnConnected = OnConnected,
+                OnDisconnect = OnDisconnect,
+                OnFailedConnection = OnFailedConnection,
+                OnReceive = OnReceive
+            };
+            gtClient = new GazeTrackerClient.Client();
+        }
+
+        #endregion
+
+        #region PRIVATE METHODS
+        
+        // Gestione eventi GazeTrackerClient
+        private void OnErrorOccured(object sender, StringEventArgs e)
+        {
+            var message = e.Param;
+            Exception ex = new Exception(message);
+            NotifyError(ex);
+        }
+
+        private void OnCalibrationPointChange(string e)
+        {
+            char[] seperator = { ' ' };
+            string[] coordinates = e.Split(seperator, 2);
+            string parameter1 = coordinates[0];
+            string parameter2 = coordinates[1];
+
+            char[] seperator2 = { ':' };
+            string[] coordinatex = parameter1.Split(seperator2, 2);
+            int relwidth = Int32.Parse(coordinatex[1]);
+
+            string[] coordinatey = parameter2.Split(seperator2, 2);
+            int relheight= Int32.Parse(coordinatey[1]);
+
+            int finalPointX = relwidth - (screenWidth - receivedWidth) / 2;
+            int finalPointY = relheight - (screenHeight - receivedHeight) / 2;
+
+            string message = finalPointX + " " + finalPointY;
+            SendMessage("CAL_POINT", message);
+        }
+
+        private void OnCalibrationEnd(int e)
+        {
+            SendMessage("CAL_END", " " + e);
+        }
+
+        private void OnCalibrationResult(int e)
+        {
+            SendMessage("CAL_RESULT", " " + e);
+        }
+
+        private void OnGazeData(GazeData data)
+        {
+            if(onStream)
+                SendMessage("GAZE_DATA", data.GazePositionX + " " + data.GazePositionY);
+        }
+
+        // Gestione eventi WebSocketClient
+        private void OnFailedConnection(UserContext context)
+        {
+            string errorMessage = "Connessione al software fallita";
+            Exception ex = new Exception(errorMessage);
+            NotifyError(ex);
+        }
+
+        private void OnConnected(UserContext context)
+        {
+            connected = true;
+            StringEventArgs message = new StringEventArgs(connected.ToString());
+            ConnectionChange(this, message);
+        }
+
+        private void OnDisconnect(UserContext context)
+        {
+            connected = false;
+            StringEventArgs message = new StringEventArgs(connected.ToString());
+            ConnectionChange(this, message);
+        }
+
+        ///<summary>
+        /// Event fired when a data is received from the Alchemy Websockets server instance.
+        /// Parses data as JSON and calls the appropriate message or sends an error message.
+        /// </summary>
+        /// <param name="context">The user's connection context</param>
+        public static void OnReceive(UserContext context)
+        {
+            try
+            {
+                cont = context;
+                var json = context.DataFrame.ToString();
+                Console.WriteLine(json);
+                dynamic obj = JsonConvert.DeserializeObject(json);
+                Console.WriteLine(obj.TYPE);
+                if ((obj.TYPE) == ("IDENTIFICATION"))
+                {
+                    string name = "EyeTrackerClient";
+                    var r = new Response { TYPE = "IDENTIFICATION", DATA = name };
+                    context.Send(JsonConvert.SerializeObject(r));
+                }
+
+                if ((obj.TYPE) == ("IDENTIFICATION_COMPLETE"))
+                {
+                    // devo recuperare ID salvato su file ini per esempio!
+                    // se è vuoto restituisco stringa vuota
+                    // se c'è invece restituisco stringa con id: TYPE: MACHINE_ID, DATA: valore
+
+                    XmlReader reader = XmlReader.Create("Settings.xml");
+                    string ID = "";
+                    while (reader.Read())
+                    {
+                        if (reader.NodeType == XmlNodeType.Element && reader.Name == "EyeTracker")
+                        {
+                            ID = reader.GetAttribute(0);
+                        }
+                    }
+                    if (ID.Equals(""))
+                    {
+                        var r = new Response { TYPE = "MACHINE_ID", DATA = "" };
+                        context.Send(JsonConvert.SerializeObject(r));
+                    }
+                    else
+                    {
+                        var r = new Response { TYPE = "MACHINE_ID", DATA = ID };
+                        context.Send(JsonConvert.SerializeObject(r));
+                    }
+                }
+
+                if ((obj.TYPE) == ("OFFSET_CALCULATION"))
+                {
+                    if ((obj.TODO) == ("true"))
+                    {
+                        var r = new Response { TYPE = "START_OFFSET_CALCULATION", DATA = "" };
+                        context.Send(JsonConvert.SerializeObject(r));
+                    }
+                    else
+                    {
+                        var r = new Response { TYPE = "READY_TO_PLAY", DATA = "" };
+                        context.Send(JsonConvert.SerializeObject(r));
+                    }
+                }
+
+                if ((obj.TYPE) == ("CALCULATING"))
+                {
+                    // spedisco pacchetto TYPE: CALCULATING, DATA: data corrente in mill dal 1970
+                    DateTime baseDate = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+                    TimeSpan diff = DateTime.UtcNow - baseDate;
+                    var r = new Response { TYPE = "CALCULATING", DATA = (long)(diff.TotalMilliseconds) };
+                    context.Send(JsonConvert.SerializeObject(r));
+                }
+
+                if ((obj.TYPE) == ("OFFSET_CALCULATION_COMPLETE"))
+                {
+                    int id = obj.MACHINE_ID;
+                    //devo salvare valore ID dell'EyeTracker dentro il file xml
+                    XmlWriterSettings settings = new XmlWriterSettings();
+                    settings.Indent = true;
+                    XmlWriter writer = XmlWriter.Create("Settings.xml", settings);
+                    writer.WriteStartDocument();
+                    writer.WriteComment("This file is generated by the program.");
+                    writer.WriteStartElement("EyeTracker");
+                    writer.WriteAttributeString("ID", id.ToString());
+                    writer.WriteEndElement();
+                    writer.WriteEndDocument();
+                    writer.Flush();
+                    writer.Close();
+                    var r = new Response { TYPE = "READY_TO_PLAY", DATA = "" };
+                    context.Send(JsonConvert.SerializeObject(r));
+                }
+
+                if ((obj.TYPE) == ("TRAINING_SESSION"))
+                {
+                    int pID = obj.PATIENT_ID;
+                    if (IDSession == pID )
+                    {
+                        var r = new Response { TYPE = "TRAINING_SESSION", DATA = "false" };
+                        context.Send(JsonConvert.SerializeObject(r));
+                    }
+                    else
+                    {
+                        IDSession = pID;
+                        var r = new Response { TYPE = "TRAINING_SESSION", DATA = "true" };
+                        context.Send(JsonConvert.SerializeObject(r));
+                    }
+                }
+
+                if ((obj.TYPE) == ("SCREEN_MEASURES"))
+                {
+                    receivedWidth = obj.SCREEN_WIDTH;
+                    receivedHeight = obj.SCREEN_HEIGHT;
+                    gtClient.Calibration.AreaSize(receivedWidth, receivedHeight);
+                }
+
+                if ((obj.TYPE) == ("START_TRAINING"))
+                {
+                    CalibrationParameters parametri = new CalibrationParameters();
+                    parametri.NumberOfPoints = obj.POINTS;
+                    parametri.PointDiameter = obj.POINT_DIAMETER;
+                    parametri.PointDuration = obj.POINT_DURATION;
+                    parametri.PointTransitionDuration = obj.TRANSITION_DURATION;
+                    gtClient.Calibration.CalibrationParameters(parametri);
+                    gtClient.Calibration.Start();
+                }
+
+                if ((obj.TYPE) == ("TRAINING_VALIDATION"))
+                {
+                    if ((obj.DATA) == "true")
+                        gtClient.Calibration.Validate();
+                    else
+                        gtClient.Calibration.Abort();
+                }
+
+                if ((obj.TYPE) == ("START_WORKING"))
+                {
+                    // Devo Partire tra START_TIME - ora attuale millisecondi
+                    gtClient.Stream.StreamStart();
+                    onStream = true;
+                }
+
+                if ((obj.TYPE) == ("STOP_GAME"))
+                {
+                    gtClient.Stream.StreamStop();
+                    onStream = false;
+                }
+
+
+            }
+            catch (Exception e) // Bad JSON! For shame.
+            {
+                var r = new Response { TYPE = "Errore: ", DATA = new { e.Message } };
+
+                context.Send(JsonConvert.SerializeObject(r));
+            }
+        }
+
+        /// <summary>
+        /// Broadcasts an error message to the client who caused the error
+        /// </summary>
+        /// <param name="errorMessage">Details of the error</param>
+        /// <param name="context">The user's connection context</param>
+        private static void SendError(string errorMessage, UserContext context)
+        {
+            var r = new Response { TYPE = errorMessage, DATA = new { Message = errorMessage } };
+
+            context.Send(JsonConvert.SerializeObject(r));
+        }
+
+        /// <summary>
+        /// Broadcasts a message to the client (Before you MUST receive a packege to set the UserContext cont)
+        /// </summary>
+        /// <param name="type">Message type</param>
+        /// <param name="data">Message data</param>
+        private static void SendMessage(string type, string data)
+        {
+            var r = new Response { TYPE = type, DATA = data };
+            if (cont != null)
+            cont.Send(JsonConvert.SerializeObject(r));
+        }
+
+        /// <summary>
+        /// Notify an error to the UI
+        /// </summary>
+        private void NotifyError(Exception ex)
+        {
+            e = new ErrorEventArgs(ex);
+            if (Error != null)
+            {
+                Error(this, e);
+            }
+        }
+
+        /// <summary>
+        /// Connect the client to the GazeTracker server
+        /// </summary>
+        private void ConnectGazeClient()
+        {
+            gtClient.IPAddress = System.Net.IPAddress.Parse("127.0.0.1");
+            gtClient.PortReceive = 6666;
+            gtClient.PortSend = 5555;
+            try
+            {
+                gtClient.Connect();
+            }
+            catch (Exception ex)
+            {
+                NotifyError(ex);
+            }
+
+        }
+
+        /// <summary>
+        /// Disconnect the client from the GazeTracker server
+        /// </summary>
+        private static void DisconnectGazeClient()
+        {
+            gtClient.Disconnect();
+        }
+
+        /// <summary>
+        /// Create the xml file where saving EyeTracker ID; Default ID=""
+        /// </summary>
+        private static void createXml()
+        {
+            if (!File.Exists("Settings.xml"))
+            {
+                XmlWriterSettings settings = new XmlWriterSettings();
+                settings.Indent = true;
+                XmlWriter writer = XmlWriter.Create("Settings.xml", settings);
+                writer.WriteStartDocument();
+                writer.WriteComment("This file is generated by the program.");
+                writer.WriteStartElement("EyeTracker");
+                writer.WriteAttributeString("ID", "");
+                writer.WriteEndElement();
+                writer.WriteEndDocument();
+                writer.Flush();
+                writer.Close();
+            }
+        }
+
+        #endregion
+
+        #region PUBLIC METHODS
+
+        public void WSConnect()
+        {
+            
+            if(!gtClient.IsRunning)
+                ConnectGazeClient();
+            wsClient.Connect();
+
+        }
+
+        public void WSDisconnect()
+        {
+            //DisconnectGazeClient();
+            wsClient.Disconnect();
+        }
+
+        public bool WSIsConnected()
+        {
+            return connected;
+        }
+
+        public GazeTrackerClient.Client getClient()
+        {
+            return gtClient;
+        }
+
+        public void SetScreenResolution(int width, int height)
+        {
+            screenWidth = width;
+            screenHeight = height;
+        }
+
+        public string log()
+        {
+            return logInfo;
+        }
+
+        #endregion
+
+        #region OBJECTS
+
+        /// <summary>
+        /// Defines the response object to send back to the client
+        /// </summary>
+        public class Response
+        {
+            public string TYPE { get; set; }
+            public dynamic DATA { get; set; }
+        }
+
+        #endregion
+
+    }
+}
